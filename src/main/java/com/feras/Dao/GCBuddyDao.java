d04ebaca6679a763d5904c28b24c129bb03f30a6
ajboard13 <aaronjboard@live.com>
2017-08-07 14:57:35 -0400

    created dao interface

--- src/main/java/com/feras/Dao/GCBuddyDao.java
+++ src/main/java/com/feras/Dao/GCBuddyDao.java
@@ -1,10 +1,30 @@
 package com.feras.Dao;
 
+import com.feras.Models.MenteesEntity;
+import com.feras.Models.MentorsEntity;
+import com.feras.Models.UsersEntity;
+
+import java.util.ArrayList;
+
 /*
 Aaron Board
  */
 public interface GCBuddyDao {
     public static final int HIBERNATE_DAO = 1;
 
-
+    public ArrayList<UsersEntity> getAllUsers();
+    public ArrayList<UsersEntity> getAllMentees();
+    public ArrayList<UsersEntity> getAllMentors();
+    public UsersEntity getUser(int userId);
+    public UsersEntity getMentor(int userId);
+    public UsersEntity getMentee(int userId);
+    public void updateUser(UsersEntity user);
+    public void updateMentor(MentorsEntity mentor);
+    public void updateMentee(MenteesEntity mentee);
+    public void deleteUser(int userId);
+    public void deleteMentor(int userId);
+    public void deleteMentee(int userId);
+    public void addUser(UsersEntity user);
+    public void addMentor(MentorsEntity mentor);
+    public void addMentee(MenteesEntity mentee);
 }
